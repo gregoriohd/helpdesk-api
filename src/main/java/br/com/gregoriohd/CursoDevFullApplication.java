17fd6632870edd6a9a5f4a5329b183243072d509
Gregorio Santos <gregoriohd@discovery.gregoriohd.com.br>
2025-09-13 19:51:35 -0300

    commit inicial

---
+++ src/main/java/br/com/gregoriohd/CursoDevFullApplication.java
@@ -0,0 +1,13 @@
+package br.com.gregoriohd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDevFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDevFullApplication.class, args);
+	}
+
+}

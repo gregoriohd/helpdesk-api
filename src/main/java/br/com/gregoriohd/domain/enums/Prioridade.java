33899007aa99348f1aa5f6a6ae666141b074b6ed
Gregorio Santos <gregoriohd@discovery.gregoriohd.com.br>
2025-09-14 18:29:10 -0300

    Criando domain enumerados: PERFIL, PRIORIDADE, STATUS

---
+++ src/main/java/br/com/gregoriohd/domain/enums/Prioridade.java
@@ -0,0 +1,35 @@
+package br.com.gregoriohd.domain.enums;
+
+public enum Prioridade {
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+
+	private Integer codigo;
+	private String descricao;
+
+	private Prioridade(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Prioridade toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Prioridade p : Prioridade.values()) {
+			if(cod.equals(p.getCodigo())) {
+				return p;
+			}
+		}
+		throw new IllegalArgumentException("Prioridade invalido.");
+	}
+
+}
---
+++ src/main/java/br/com/gregoriohd/domain/enums/Status.java
@@ -0,0 +1,35 @@
+package br.com.gregoriohd.domain.enums;
+
+public enum Status {
+	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+
+	private Integer codigo;
+	private String descricao;
+
+	private Status(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Status toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Status s : Status.values()) {
+			if(cod.equals(s.getCodigo())) {
+				return s;
+			}
+		}
+		throw new IllegalArgumentException("Status invalido.");
+	}
+
+}

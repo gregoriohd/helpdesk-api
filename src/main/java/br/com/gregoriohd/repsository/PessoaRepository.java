74c1deaf0f0a324cc2d7a91be91753531ea35137
Gregorio Santos <gregoriohd@gmail.com>
2025-10-28 20:04:44 -0300

    aprimorando as exceptions

---
+++ src/main/java/br/com/gregoriohd/repsository/PessoaRepository.java
@@ -0,0 +1,13 @@
+package br.com.gregoriohd.repsository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gregoriohd.domain.Pessoa;
+import java.util.Optional;
+
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
+}
